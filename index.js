221ea4f20f20ccb3e64f15bc5141effbff3c77d4
Parker Parham <parkerparham@gmail.com>
2022-07-21 15:51:08 -0400

    commit tree cab4e75d9aaea154a04f9e413c6f61f62e90b7a2

--- index.js
+++ index.js
@@ -10,7 +10,28 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+const titleCased = () => { //this line declares function with arrow notation
+ return tutorials.map((tutorial) => { //here we are mapping tutorial and starting function of how we want to map it 
+    const titles = tutorial.split(" "); //for each element of tutorial(the titles), its splitting up each title into its own array
+    const capTitles = titles.map((wordArray) => { //so now we are mapping through the split words in the titles
 
-const titleCased = () => {
-  return tutorials
+      return wordArray[0].toUpperCase() + wordArray.substring(1) //this is capitalizing the first letter in each word and putting the rest of the letters in each word with it
+    }).join(" "); //this join those words back up into sentences. it keeps mapping through all the elements until it is finished
+    // console.log(capTitles)
+    return capTitles;
+  });
+  // const foo = tutorials.map(x => x.toLowerCase());
+  // console.log(words)
+  // console.log(newWords)
 }
+
+// const titleCased = () => {
+//   return tutorials.map((tutorial) => {
+//     const titlesByWord = tutorial.split(" ");
+//     const capTitles = titlesByWord.map((wordInArray) => {
+//       return wordInArray[0].toUpperCase() + wordInArray.substring(1)
+//     }).join(" ")
+//     return capTitles
+//   })
+// }
+titleCased()
